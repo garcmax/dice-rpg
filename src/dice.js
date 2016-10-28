16b4cdccc0eb1e1cf244ee6e416aec3926142fcb
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-10-28 16:09:14 +0200

    finished

--- src/dice.js
+++ src/dice.js
@@ -3,20 +3,33 @@
 const random = require("random-js");
 const engine = random.engines.mt19937().autoSeed();
 
-export function rolled(dices) {
-    let rolls = dices.split(/\s/);
+export function rolled(input) {
+    let rolls = input.split(/\s/);
     let results = {};
-    let regexp = /^(\d|d)d{0,1}([2468]|10|20|100)$/gi;
-    console.log(`split : ${rolls}`);
+    let regexp = /^(\d|d)d{0,1}([2468]|10|12|20|100)$/i;
     for (let i = 0; i < rolls.length; i++) {
-        let matchs = regexp.exec(rolls[i]);
-        console.log(`match : ${matchs} from ${rolls[i]}`);
-        let times = /^\d+$/.test(matchs[1]) ? matchs[1] : "1";
-        let dice = matchs[2];
-        console.log(`throw : ${times}d${dice}`);
-        let roll = random.dice(dice, times);
-        let result = roll(engine);
-        results[dice] = result;
+        let dices;
+        try {
+            dices = extractDices(rolls[i], regexp);
+        } catch(e) {
+            return { "error": "Bad Input" };
+        }
+        results[dices.dice] = rollDices(dices);
     }
     return results;
+}
+
+function extractDices(roll, regexp) {
+    let matchs = regexp.exec(roll);
+    if (matchs == null) {
+        throw "1";
+    }
+    let times = /^\d+$/.test(matchs[1]) ? matchs[1] : "1";
+    let dice = matchs[2];
+    return {"dice" : dice, "times": times}
+}
+
+function rollDices(dices) {
+    let roll = random.dice(dices.dice, dices.times);
+    return roll(engine);    
 }
--- test/diceSpec.js
+++ test/diceSpec.js
@@ -75,11 +75,10 @@ describe('throwing one dice twice', function () {
 describe('throwing two dices once', function () {
 
     var rng;
-    var rngResult = [4,3]
     beforeEach(function () {
         rng = sinon.stub(random, "dice").returns(
             function (engine) {
-                return rngResult;
+                return [4];
             }
         );
     });
@@ -90,9 +89,66 @@ describe('throwing two dices once', function () {
 
     it('should get the result from d6 d4', function (done) {
         let result = dice.rolled("d6 d4");
+        rng.calledTwice.should.be.ok();
         rng.calledWith("6", "1").should.be.true();
         rng.calledWith("4", "1").should.be.true();
-        result.should.deepEqual({6: rngResult, 4: rngResult});
+        done();
+    });
+});
+
+describe('throwing two dices multiple times', function () {
+
+    var rng;
+    beforeEach(function () {
+        rng = sinon.stub(random, "dice").returns(
+            function (engine) {
+                return [4];
+            }
+        );
+    });
+
+    afterEach(function () {
+        random.dice.restore();
+    });
+
+    it('should get the result from 3d6 2d4', function (done) {
+        let result = dice.rolled("3d6 2d4");
+        rng.calledTwice.should.be.ok();
+        rng.calledWith("6", "3").should.be.true();
+        rng.calledWith("4", "2").should.be.true();
+        done();
+    });
+});
+
+describe('throwing multiple dices multiple times', function () {
+
+    it('should get the result from 1d100 2d20 3d6 2d4', function (done) {
+        let result = dice.rolled("1d100 2d20 3d6 2d4");        
+        result[100].length.should.equal(1);
+        result[20].length.should.equal(2);
+        result[6].length.should.equal(3);
+        result[4].length.should.equal(2);
+        done();
+    });
+});
+
+describe('Error catching', function () {
+
+    it('should get the error from badly formed input : d11', function (done) {
+        let result = dice.rolled("d11");        
+        result.should.deepEqual({"error" : "Bad Input"})        
+        done();
+    });
+
+    it('should get the error from badly formed input : d112d6', function (done) {
+        let result = dice.rolled("d112d6");        
+        result.should.deepEqual({"error" : "Bad Input"})        
+        done();
+    });
+
+     it('should get the error from badly formed input : toto', function (done) {
+        let result = dice.rolled("toto");        
+        result.should.deepEqual({"error" : "Bad Input"})        
         done();
     });
 });
